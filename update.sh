9b5d79cfd2ef5cfb9986c751dc45b8604298b8b1
Zac Kleinpeter <zac@kleinpeter.org>
2012-06-07 06:03:21 -0500

    moving to the swamp

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ if [ ! -f $VIM_UPDATE_LOCK ]
 then
   touch $VIM_UPDATE_LOCK
   cd ~/.vim
-  git pull > /dev/null && vim -e -s -u ~/.vim/bundles.vim +BundleClean! +BundleInstall! +qall > /dev/null
+  git remote set-url origin git://github.com/cajun/swamp.git
+  git pull origin master > /dev/null && vim -e -s -u ~/.vim/bundles.vim +BundleClean! +BundleInstall! +qall > /dev/null
   rm $VIM_UPDATE_LOCK
 fi
