34cae53bea52e840b8a07655423a7181ccc32911
Zac <zac@kleinpeter.org>
2012-06-07 06:13:59 -0500

    updating update script

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ if [ ! -f $VIM_UPDATE_LOCK ]
 then
   touch $VIM_UPDATE_LOCK
   cd ~/.vim
-  git pull origin master > /dev/null && vim -e -s -u ~/.vim/bundles.vim $VIM_UPDATE_LOCK +BundleClean! +BundleInstall! +qall > /dev/null
+  git pull > /dev/null && vim -e -s +BundleClean! +BundleInstall! +qall > /dev/null
   rm $VIM_UPDATE_LOCK
 fi
