8cbebfd079e7f00149f389f2b7c755897fe83cb1
Zac Kleinpeter <zac@kleinpeter.org>
2012-06-08 08:44:15 -0500

    Update master

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ if [ ! -f $VIM_UPDATE_LOCK ]
 then
   touch $VIM_UPDATE_LOCK
   cd ~/.vim
+  git remote set-url origin git://github.com/cajun/voodoo.git
   git pull > /dev/null && vim -e -s +BundleClean! +BundleInstall! +qall > /dev/null
   rm $VIM_UPDATE_LOCK
 fi
