8a790b25955e3b263b88997106100cc15cb05567
Zac <zac@kleinpeter.org>
2013-06-05 09:51:22 -0500

    Updating install script

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 cd ~
-mv ~/.vim ~/.vim.old
-mv ~/.vimrc ~/.vimrc.old
+if [[ -d !~/.vim ]]
+then
+  mv ~/.vim ~/.vim.old
+  mv ~/.vimrc ~/.vimrc.old
+fi
 
 git clone git://github.com/cajun/voodoo.git ~/.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 ln -sf ~/.vim/.vimrc
 
-vim -u ~/.vim/bundles.vim +BundleInstall +qall
+vim --noplugin +BundleInstall! +":w >> ~/.vim/history.log" +qa!
+
+
